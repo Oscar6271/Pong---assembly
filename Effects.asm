61fbe153b631c5d588c0b9de4bf20b2a6fefd897
Oscar6271 <115797713+Oscar6271@users.noreply.github.com>
2025-03-11 21:10:34 +0100

    Update Effects.asm

--- Effects.asm
+++ Effects.asm
@@ -4,19 +4,79 @@
  *  Created: 2025-02-26 11:33:49
  *   Author: ludbr478
  */ 
- //r17 bitm�nster
+ //r17 bitmönster
 
  //BGR rad
  START_EFFECT:
+	ldi		r19, 0b00000100 // Tre sista bitarna ändrar färg, RGB
+	call	EFFECT
+
+	ldi		r16,200
+	ldi		r17,100
+	call	EFFECT_DELAY
+	cli
+	call	BEEP
+	sei
+
+	ldi		r19, 0b00000110 // Tre sista bitarna ändrar färg, RGB
+	call	EFFECT
+
+	ldi		r16,200
+	ldi		r17,100
+	call	EFFECT_DELAY
+	cli
+	call	BEEP
+	sei
+
+	ldi		r19, 0b00000010 // Tre sista bitarna ändrar färg, RGB
+	call	EFFECT
+
+	ldi		r16,200
+	ldi		r17,100
+	call	EFFECT_DELAY
+	cli
+	call	BEEP
+	sei
+
+	ret
+
+GAME_OVER_EFFECT:
+	
+	call	EFFECT
+
+	ldi		r16,100
+	ldi		r17,100
+	call	EFFECT_DELAY
+
+	cli
+	call	BEEP
+	sei
+
+	call	EFFECT
+
+	ldi		r16,100
+	ldi		r17,100
+
+	call	EFFECT_DELAY
+
+	cli
+	call	BEEP
+	sei
+
+	ret
+
+ EFFECT:
 	ldi		ZL,LOW(VMEM)
+	ldi		ZH,HIGH(VMEM)
+	
 	ldi		r18,8
 	ldi		r17,$FF
 	clr		r21
-	ldi		r19, 0b00001010 // Tre sista bitarna �ndrar f�rg, RGB
-START_EFFECT_OUTERLOOP:
-	ldi		r16,VMEM_COLOR_SECTION_SZ	;3
+	
+EFFECT_OUTERLOOP:
+	ldi		r16,3			;VMEM_COLOR_SECTION_SZ	;3
 	mov		r20,r19
-START_EFFECT_INNERLOOP:
+EFFECT_INNERLOOP:
 	sbrc	r20,0
 	st		Z+,r17
 	sbrs	r20,0
@@ -26,28 +86,34 @@ START_EFFECT_INNERLOOP:
 
 	lsr		r20
 	dec		r16
-	brne	START_EFFECT_INNERLOOP
+	brne	EFFECT_INNERLOOP
 	inc		ZL
 	dec		r18
-	brne	START_EFFECT_OUTERLOOP
+	brne	EFFECT_OUTERLOOP
 
-	ldi		r16,100
-	ldi		r17,50
-	call	BEEP
+	call	ERASE_VMEM
+	
 	ret
 
 
 EFFECT_DELAY:
 	push	r16
 	push	r17
-	ldi		r16,100		; Decimal bas
+	push	r18
+	ldi		r18,4
+EFFECT_LONG_DELAY:
+	
+	ldi		r16,255		; Decimal bas
 EFFECT_delayYttreLoop:
-	ldi		r17,$FF
+	ldi		r17,$A1
 EFFECT_delayInreLoop:
 	dec		r17
 	brne	EFFECT_delayInreLoop
 	dec		r16
 	brne	EFFECT_delayYttreLoop
+	dec		r18
+	brne	EFFECT_LONG_DELAY
+	pop		r18
 	pop		r17
 	pop		r16
 	ret
