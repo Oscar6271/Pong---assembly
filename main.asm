b22dd41ee87323aa71368c26b6d06a6cfcd5c31d
Oscar6271 <115797713+Oscar6271@users.noreply.github.com>
2025-03-11 21:11:13 +0100

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 	.org	$0
 	jmp		START_GAME
 	
-	.org	INT0addr
+	.org	OVF0addr
 	jmp		MUX
 	
 .include "VMEM.asm"
@@ -9,17 +9,62 @@
 .include "speaker.asm"
 .include "WRITE_7SEG.asm"
 
+.include "Effects.asm"
+.include "Start_Button.asm"
+
+.include "spelare.asm"
+
 START_GAME:
 	ldi		r16,LOW(RAMEND)
 	out		SPL,r16
 	ldi		r16,HIGH(RAMEND)
 	out		SPH,r16
-
+	
 	call	HW_INIT
+
+	ldi		r17, 3
+	call	START_BUTTON
+	call	START_EFFECT
+	
+	call	PLAYER_INIT
 RUN_GAME:
+	call	Joystick_Input
+
+	call	DELAY_GAME
 	call	CHANGE_BALL_POS
+
+
 	jmp		RUN_GAME	
 
+	
 
-GAME_OVER:
-	jmp		GAME_OVER
+DELAY_GAME:
+	push	r17
+	ldi		r19,$10
+DELAY_GAME_Yttre_YTTRE_LOOP:
+	ldi		r18, $FF
+DELAY_GAME_YttreLoop:
+	ldi		r17, $FF
+DELAY_GAME_InreLoop:
+	dec		r17
+	brne	DELAY_GAME_InreLoop
+	dec		r18
+	brne	DELAY_GAME_YttreLoop
+	dec		r19
+	brne	DELAY_GAME_Yttre_YTTRE_LOOP
+	pop		r17
+	ret
+
+
+LEFT_WON:
+	ldi		r19, 0b00000100 // Tre sista bitarna ändrar färg, RGB
+	call	GAME_OVER_EFFECT
+
+	jmp		START_GAME
+
+
+RIGHT_WON:
+	ldi		r19, 0b00000001 // Tre sista bitarna ändrar färg, RGB
+	call	GAME_OVER_EFFECT
+
+	jmp		START_GAME
