3982d897549e1715bac73a7cb3dbe22776f88259
Oscar6271 <115797713+Oscar6271@users.noreply.github.com>
2025-03-11 21:10:54 +0100

    Update VMEM.asm

--- VMEM.asm
+++ VMEM.asm
@@ -1,18 +1,18 @@
-	/*Funktioner som andra program får använda (resterande är privata):
+	/*Funktioner som andra program fr anvnda (resterande r privata):
 
-		HW_INIT(): sätt start pos och färg på boll och spelare. sätt register för SPI
+		HW_INIT(): stt start pos och frg p boll och spelare. stt register fr SPI
 
-		ERASE_VMEM(): Tar bort allt i VMEM förutom anod (mönstret för raden)
+		ERASE_VMEM(): Tar bort allt i VMEM frutom anod (mnstret fr raden)
 
-		RESET_VMEM() -> r19=BSPEEDY, r16=BSPEEDX: sparar bollens nya position(5,4), rensar och sätter tillbaka spelare och boll
+		RESET_VMEM() -> r19=BSPEEDY, r16=BSPEEDX: sparar bollens nya position(5,4), rensar och stter tillbaka spelare och boll
 
-		SET_RIGHT_PLAYER(): sätter bitmönster för högra spelare
-		SET_LEFT_PLAYER(): sätter bitmönster för vänstra spelare
-		SET_BALL(): sätter bitmönster för bollen
+		SET_RIGHT_PLAYER(): stter bitmnster fr hgra spelare
+		SET_LEFT_PLAYER(): stter bitmnster fr vnstra spelare
+		SET_BALL(): stter bitmnster fr bollen
 
-		CLEAR_RIGHT_PLAYER: tar bort höger spelare
-		CLEAR_LEFT_PLAYER: tar bort vänster spelare
-		CLEAR_BALL: tar bort bollen från videominnet
+		CLEAR_RIGHT_PLAYER: tar bort hger spelare
+		CLEAR_LEFT_PLAYER: tar bort vnster spelare
+		CLEAR_BALL: tar bort bollen frn videominnet
 
 	*/	
 	.equ	RED	= 2
@@ -27,19 +27,10 @@
 	.equ	MOSI = PB5
 	.equ	SCK = PB7
 
+
 	.dseg
 .org SRAM_START
-	VMEM:		.byte VMEM_SZ		//Blå, Grön, Röd, Rad
-
-	LPOSX:		.byte 1
-	LPOSY:		.byte 1
-	LCOLOR:		.byte 1
-	LPOINT:		.byte 1
-
-	RPOSX:		.byte 1
-	RPOSY:		.byte 1
-	RCOLOR:		.byte 1
-	RPOINT:		.byte 1
+	VMEM:		.byte VMEM_SZ		//Bl, Grn, Rd, Rad
 	
 	BPOSX:		.byte 1
 	BPOSY:		.byte 1
@@ -47,28 +38,49 @@
 	BSPEEDX:	.byte 1
 	BSPEEDY:	.byte 1
 
+.org SRAM_START+100
+	RPOSY:		.byte 1
+
+.org SRAM_START+110
+	RCOLOR:		.byte 1
+
+.org SRAM_START+120
+	RPOINT:		.byte 1
+
+.org SRAM_START+130
+	LPOSY:		.byte 1
+
+.org SRAM_START+140
+	LCOLOR:		.byte 1
+
+.org SRAM_START+150
+	LPOINT:		.byte 1
+
+.org SRAM_START+160
 	LINE:		.byte 1	
 	.cseg
 
-//////////////////////////////////RESET_VMEM() -> r19=BSPEEDY, r16=BSPEEDX: sparar bollens nya position(5,4), rensar och sätter tillbaka spelare och boll
+//////////////////////////////////RESET_VMEM() -> r19=BSPEEDY, r16=BSPEEDX: sparar bollens nya position(4,4), rensar och stter tillbaka spelare och boll
 RESET_VMEM:
 	call	ERASE_VMEM
 	call	SET_LEFT_PLAYER
 	call	SET_RIGHT_PLAYER
-	ldi		r17,5
+	ldi		r17,4
 	sts		BPOSX,r17
 	ldi		r17,4
 	sts		BPOSY,r17
-	ldi		r16,1
+	
+	sts		BSPEEDX,r20
 	clr		r19
+	sts		BSPEEDY,r19
 	ret
 
-//////////////////////////////////SET_RIGHT_PLAYER(): sätter bitmönster för högra spelare
+//////////////////////////////////SET_RIGHT_PLAYER(): stter bitmnster fr hgra spelare
 SET_RIGHT_PLAYER:
-	ldi		ZL,LOW(RPOSX)
-	ldi		ZH,HIGH(RPOSX)
-	call	SETPOS
-	mov		r17,r16
+	ldi		r17,$01
+	ldi		ZL,LOW(VMEM)
+	ldi		ZH,HIGH(VMEM)
+
 	lds		r18,RPOSY
 	lds		r19,RCOLOR
 	call	SET_PLAYER_PRIVATE
@@ -76,12 +88,12 @@ SET_RIGHT_PLAYER:
 	ret
 
 
-//////////////////////////////////SET_LEFT_PLAYER(): sätter bitmönster för vänstra spelare
+//////////////////////////////////SET_LEFT_PLAYER(): stter bitmnster fr vnstra spelare
 SET_LEFT_PLAYER:
-	ldi		ZL,LOW(LPOSX)
-	ldi		ZH,HIGH(LPOSX)
-	call	SETPOS
-	mov		r17,r16
+	ldi		r17,$80
+	ldi		ZL,LOW(VMEM)
+	ldi		ZH,HIGH(VMEM)
+
 	lds		r18,LPOSY
 	lds		r19,LCOLOR
 	call	SET_PLAYER_PRIVATE
@@ -89,7 +101,7 @@ SET_LEFT_PLAYER:
 	ret
 
 
-//////////////////////////////////SET_BALL(): sätter bitmönster för bollen
+//////////////////////////////////SET_BALL(): stter bitmnster fr bollen
 SET_BALL:
 	ldi		ZL,LOW(BPOSX)
 	ldi		ZH,HIGH(BPOSX)
@@ -106,7 +118,7 @@ SET_BALL:
 	ret
 
 
-/////////////////////////////////CLEAR_BALL: tar bort bollen från videominnet
+/////////////////////////////////CLEAR_BALL: tar bort bollen frn videominnet
 CLEAR_BALL:
 	lds		r18,BPOSY
 	lds		r19,BCOLOR
@@ -119,7 +131,7 @@ CLEAR_BALL:
 	ret	
 
 
-//////////////////////////////////CLEAR_LEFT_PLAYER: tar bort vänster spelare
+//////////////////////////////////CLEAR_LEFT_PLAYER: tar bort vnster spelare
 CLEAR_LEFT_PLAYER:
 	push	r20
 
@@ -132,7 +144,7 @@ CLEAR_LEFT_PLAYER:
 	ret
 
 
-//////////////////////////////////CLEAR_RIGHT_PLAYER: tar bort höger spelare
+//////////////////////////////////CLEAR_RIGHT_PLAYER: tar bort hger spelare
 CLEAR_RIGHT_PLAYER:
 	push	r20
 
@@ -144,19 +156,30 @@ CLEAR_RIGHT_PLAYER:
 	pop		r20
 	ret
 
-//////////////////////////////////HW_INIT: sätt start pos och färg på boll och spelare. sätt register för SPI och annat
+//////////////////////////////////HW_INIT: stt start pos och frg p boll och spelare. stt register fr SPI och annat
 HW_INIT:
-	ldi		r17,7
-	sts		LPOSX,r17
+	call	ERASE_VMEM
+	call	INTERRUPT_INIT
+	call	SPI_INIT
 
-	ldi		r17,6
+	ldi		r17,$FE
+	call	SET_ANOD
+
+	clr		r20
+
+	call	RIGHT8_WRITE
+	call	LEFT8_WRITE
+
+	sts		LINE,r20
+
+	ret
+
+PLAYER_INIT:
+	ldi		r17,5
 	sts		LPOSY,r17
 
 	ldi		r17,RED
 	sts		LCOLOR,r17
-
-	ldi		r17,0
-	sts		RPOSX,r17
 
 	ldi		r17,5
 	sts		RPOSY,r17
@@ -164,7 +187,7 @@ HW_INIT:
 	ldi		r17,BLUE
 	sts		RCOLOR,r17
 
-	ldi		r17,5
+	ldi		r17,3
 	sts		BPOSX,r17
 
 	ldi		r17,4
@@ -173,7 +196,7 @@ HW_INIT:
 	ldi		r17,GREEN
 	sts		BCOLOR,r17
 
-	ldi		r17,-1
+	ldi		r17,1
 	sts		BSPEEDX,r17
 
 	clr		r17
@@ -184,11 +207,6 @@ HW_INIT:
 
 	call	ERASE_VMEM
 
-	ldi		r17,$FE
-	call	SET_ANOD
-
-	call	SPI_INIT
-
 	call	SET_LEFT_PLAYER
 	call	SET_RIGHT_PLAYER
 	call	SET_BALL
@@ -197,17 +215,26 @@ HW_INIT:
 
 //////////////////////////////////SPI_INIT(): Initierar allt med SPI
 SPI_INIT:
-	ldi		r17,(1<<MOSI) | (1<<SCK) | (1<<SS) //ändra så att man skriver MSB först istället för LSB, eller tvärtom
+	ldi		r17,(1<<MOSI) | (1<<SCK) | (1<<SS) 
 	out		DDRB,r17
 
 	ldi		r17,(1<<SPE) | (1<<MSTR) | (1<<SPR0)
 	out		SPCR,r17
-	sei
-
+	
 	ret
 
+//////////////////////////////////INTERRUPT_INIT(): Initierar timer0 fÃ¶r avbrott
+INTERRUPT_INIT:
+	ldi		r17, (1<<CS01) | (1<<CS00)
+	out		TCCR0,r17
 
-//////////////////////////////////ERASE_VMEM(): Tar bort allt i VMEM förutom anod
+	ldi		r17, (1<<TOIE0)
+	out		TIMSK, r17
+
+	sei
+	ret
+
+//////////////////////////////////ERASE_VMEM(): Tar bort allt i VMEM frutom anod
 ERASE_VMEM:
 	ldi		ZL,LOW(VMEM)
 	ldi		r18,8
@@ -224,7 +251,7 @@ ERASE_VMEM_INNERLOOP:
 	ret
 
 
-//////////////////////////////////SET_ANOD(r17=mönstret för första raden): sätter mönstret för varje rad
+//////////////////////////////////SET_ANOD(r17=mnstret fr frsta raden): stter mnstret fr varje rad
 SET_ANOD:
 	ldi		ZL,LOW(VMEM)
 	ldi		r16,VMEM_COLOR_SECTION_SZ
@@ -241,14 +268,20 @@ SET_ANOD_LOOP:
 	brne	SET_ANOD_LOOP
 	ret
 
-//////////////////////////////////MUX(): multiplexar skärmen
+//////////////////////////////////MUX(): multiplexar skrmen
 MUX:
+	push	r16
+	in		r16,SREG
 	push	r16
 	push	r17
 	push	r18
+	push	ZL
+	push	ZH
 
 	ldi		ZL,LOW(VMEM)
 	ldi		ZH,HIGH(VMEM)
+
+	sbi		PORTB,SS
 
 	lds		r17,LINE
 	add		ZL,r17
@@ -256,12 +289,16 @@ MUX:
 	ldi		r18,4
 MUX_LOOP:
 	ld		r16,Z+
-	call	SPI_SEND
+
+	out		SPDR,r16			//SPI_SEND(r16=data): skickar data till diodmatrisen
+WAIT_TRANSFER:
+	sbis	SPSR,SPIF
+	jmp		WAIT_TRANSFER
+
 	inc		r17
 	dec		r18
 	brne	MUX_LOOP
 
-	sbi		PORTB,SS
 	cbi		PORTB,SS
 
 	cpi		r17,32
@@ -270,25 +307,19 @@ MUX_LOOP:
 LINE_OK:
 	sts		LINE,r17
 
+	pop		ZH
+	pop		ZL
 	pop		r18
 	pop		r17
 	pop		r16
+	out		SREG,r16
+	pop		r16
 
-	ret
-
-
-//////////////////////////////////SPI_SEND(r16=data): skickar data till diodmatrisen
-SPI_SEND:
-	out		SPDR,r16
-WAIT_TRANSFER:
-	sbis	SPSR,SPIF
-	jmp		WAIT_TRANSFER
-
-	ret
+	reti
 
 
-//////////////////////////////////CLEAR_PLAYER(r20=0b11111110 om man vill ta bort högra spelaren, 0b01111111 för vänstra,
-											 //r18=spelarens y-pos, r19=spelarens färg): tar bort spelaren från spelplanen
+//////////////////////////////////CLEAR_PLAYER(r20=0b11111110 om man vill ta bort hgra spelaren, 0b01111111 fr vnstra,
+											 //r18=spelarens y-pos, r19=spelarens frg): tar bort spelaren frn spelplanen
 CLEAR_PLAYER:
 	ldi		ZL,LOW(VMEM)
 	call	CHECK_Y_POS
@@ -303,7 +334,7 @@ CLEAR_PLAYER_LOOP:
 	ret
 
 
-//////////////////////////////////SETPOS(Z=spelarens x-position, r18=1 för högerspelare, 0 för vänster, r19=färg)->r17=nya bitmönstret för raden
+//////////////////////////////////SETPOS(Z=spelarens x-position, r18=1 fr hgerspelare, 0 fr vnster, r19=frg)->r17=nya bitmnstret fr raden
 SETPOS:
 	ld		r17,Z+
 	call	SETBIT
@@ -311,24 +342,24 @@ SETPOS:
 
 	ldi		ZL,LOW(VMEM)
 	add		r17,r19			
-	add		ZL,r17			//sätter Z på VMEM+(y.pos)+färg
-	ld		r17,Z			//laddar in det värdet. Kan bli fel, kanske behöver CHECK_Y_POS här
+	add		ZL,r17			//stter Z p VMEM+(y.pos)+frg
+	ld		r17,Z			//laddar in det vrdet. Kan bli fel, kanske behver CHECK_Y_POS hr
 
-	ldi		ZL,LOW(VMEM)	//behövs för att man ska starta från början av VMEM, annars blir det fel i CHECK_Y_POS
+	ldi		ZL,LOW(VMEM)	//behvs fr att man ska starta frn brjan av VMEM, annars blir det fel i CHECK_Y_POS
 	or		r17,r16
 	ret
 
 
 
-//////////////////////////////////SET_PLAYER_PRIVATE(Z=rad och färg där spelare ska sparas): Gör spelaren till 3 dioder stor
+//////////////////////////////////SET_PLAYER_PRIVATE(Z=rad och frg dr spelare ska sparas): Gr spelaren till 3 dioder stor
 SET_PLAYER_PRIVATE:
 	call	CHECK_Y_POS
 	ldi		r21,3
 SET_PLAYER_LOOP:
 	clr		r16
 	mov		r16,r17
-	ld		r18,Z				//Behövs för att man inte ska ta bort
-	or		r16,r18				//det som finns på raden sen tidigare
+	ld		r18,Z				//Behvs fr att man inte ska ta bort
+	or		r16,r18				//det som finns p raden sen tidigare
 	st		Z,r16
 	subi	ZL,VMEM_SECTION_SZ	
 	dec		r21
@@ -337,7 +368,7 @@ SET_PLAYER_LOOP:
 	ret
 
 
-//////////////////////////////////CHECK_Y_POS(r18=Y-position,r19=färg): sätter in Z-pekaren på rätt rad och färg i VMEM
+//////////////////////////////////CHECK_Y_POS(r18=Y-position,r19=frg): stter in Z-pekaren p rtt rad och frg i VMEM
 CHECK_Y_POS:
 	lsl		r18
 	lsl		r18
@@ -349,6 +380,7 @@ CHECK_Y_POS:
 
 //////////////////////////////////SETBIT(r17=X-position)
 SETBIT:
+	clr		r16
 	ldi		r16,1
 SETBIT_LOOP:
 	dec		r17
